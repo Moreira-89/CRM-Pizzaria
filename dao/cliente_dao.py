5aacd4aa1f7600aa6c1a3b7b875066a55219f14c
AmandaAmorim <amanda.t.a99@gmail.com>
2025-05-19 00:00:35 -0300

    feat: criacao do arquivo DAO da classe Cliente

---
+++ dao/cliente_dao.py
@@ -0,0 +1,19 @@
+from firebase_admin import db
+from models.cliente import Cliente
+
+class ClienteDAO:
+    def __init__(self):
+        self.ref = db.reference('/clientes')
+
+    def criarCliente() -> str:
+        novo_cliente_ref = self.ref.push(cliente.to_dict())
+        return novo_cliente_ref.key
+    
+    def buscarPorId(self, id:str) -> Cliente:
+        snapshot = self.ref.get()
+        return  Cliente(id=id, **snapshot) if snapshot else None
+    
+    def buscarTodos(self) -> list[Cliente]:
+        snapshot = self.ref.get()
+        return [Cliente(id=key, **value) for key, value i n snapshot.items()] if snapshot else []
+    
