3b100934dc54f27ccf719b742fbfd3440398ec8c
AmandaAmorim <amanda.t.a99@gmail.com>
2025-05-30 12:55:29 -0300

    feat: adiciona Avaliacao ao menu lateral

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from views.cliente_page import cliente_page
 from views.motoboy_page import motoboy_page
+from views.avaliacao_page import avaliacao_page
 
 # Futuramente, importar outras páginas:
-#from views.avaliacao_page import avaliacao_page
 # from views.fidelidade_page import fidelidade_page
 # from views.campanha_page import campanha_page
 
@@ -17,13 +17,15 @@ st.set_page_config(
 st.sidebar.image("https://img.freepik.com/free-vector/pizza-logo-design_23-2149423871.jpg", width=150)
 st.sidebar.title("CRM Pizzaria Delivery")
 
-menu = ["Clientes", "Motoboys"]
+menu = ["Clientes", "Motoboys", "Avaliações"]
 escolha = st.sidebar.selectbox("Menu Principal", menu)
 
 if escolha == "Clientes":
     cliente_page()
 elif escolha == "Motoboys":
     motoboy_page()
+elif escolha == "Avaliações":
+    avaliacao_page()
 
 st.sidebar.markdown("---")
 st.sidebar.markdown("Desenvolvido por Amanda Taveira Amorim")
