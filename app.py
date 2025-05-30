007273db903d45b1e5f7acb8a14d2339ecb2e43a
AmandaAmorim <amanda.t.a99@gmail.com>
2025-05-30 12:03:11 -0300

    feat: adiciona aba Motoboys no menu lateral

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 import streamlit as st
 from views.cliente_page import cliente_page
-# Futuramente, importar outras páginas como motoboy_page, avaliacao_page, etc.
+from views.motoboy_page import motoboy_page
+
+# Futuramente, importar outras páginas:
+#from views.avaliacao_page import avaliacao_page
+# from views.fidelidade_page import fidelidade_page
+# from views.campanha_page import campanha_page
+
 
 st.set_page_config(
     page_title="CRM Pizzaria",
     layout="wide"
 )
 
+#Barra lateral
 st.sidebar.image("https://img.freepik.com/free-vector/pizza-logo-design_23-2149423871.jpg", width=150)
 st.sidebar.title("CRM Pizzaria Delivery")
 
-menu = ["Clientes"]
+menu = ["Clientes", "Motoboys"]
 escolha = st.sidebar.selectbox("Menu Principal", menu)
 
 if escolha == "Clientes":
     cliente_page()
+elif escolha == "Motoboys":
+    motoboy_page()
 
 st.sidebar.markdown("---")
 st.sidebar.markdown("Desenvolvido por Amanda Taveira Amorim")
