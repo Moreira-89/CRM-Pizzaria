f268aa8fb9f0cf5e8d0ddc95834f5f111b59f35b
AmandaAmorim <amanda.t.a99@gmail.com>
2025-05-30 12:49:41 -0300

    feat:  DAO do modulo Avaliacao

---
+++ dao/avaliacao_dao.py
@@ -0,0 +1,34 @@
+import uuid
+from dao.firebase_dao import FirebaseDAO
+from models.avaliacao import Avaliacao
+
+
+class AvaliacaoDAO(FirebaseDAO):
+    def __init__(self):
+        super().__init__(collection="avaliacoes")
+
+    def criar(self, avaliacao: Avaliacao):
+        if not avaliacao.id:
+            avaliacao.id = str(uuid.uuid4())
+        self.db.child(avaliacao.id).set(avaliacao.to_dict())
+        return avaliacao.id
+
+    def buscar_por_id(self, id):
+        data = self.db.child(id).get()
+        if data and data.val():
+            return Avaliacao.from_dict(data.val())
+        return None
+
+    def listar_todos(self):
+        data = self.db.get()
+        if data and data.val():
+            return [Avaliacao.from_dict(item) for item in data.val().values()]
+        return []
+
+    def atualizar(self, avaliacao: Avaliacao):
+        if not avaliacao.id:
+            raise ValueError("ID da avaliação não informado para atualização.")
+        self.db.child(avaliacao.id).update(avaliacao.to_dict())
+
+    def deletar(self, id):
+        self.db.child(id).delete()
