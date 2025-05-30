95078bccdba2c69c0ba9f1c848b91361b7defa74
AmandaAmorim <amanda.t.a99@gmail.com>
2025-05-29 22:38:28 -0300

    feat: adiciona metodos para interagir com o db

--- dao/firebase_dao.py
+++ dao/firebase_dao.py
@@ -1,9 +1,24 @@
-import pyrebase
-from config.firebase_config import firebase_config
+from config.firebase_config import FirebaseConfig
 
 
 class FirebaseDAO:
     def __init__(self, collection):
-        firebase = pyrebase.initialize_app(firebase_config)
-        self.db = firebase.database()
+        self.db = FirebaseConfig.get_instance().rtdb.child(collection)
         self.collection = collection
+
+    def criar(self, id, data):
+        self.db.child(id).set(data)
+
+    def buscar_por_id(self, id):
+        snapshot = self.db.child(id).get()
+        return snapshot if snapshot else None
+
+    def listar_todos(self):
+        snapshot = self.db.get()
+        return snapshot if snapshot else {}
+
+    def atualizar(self, id, data):
+        self.db.child(id).update(data)
+
+    def deletar(self, id):
+        self.db.child(id).delete()
