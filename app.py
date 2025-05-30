b29567d306b4b64051c8eb6abdca467a01f4ff61
AmandaAmorim <amanda.t.a99@gmail.com>
2025-05-30 16:16:52 -0300

    feat: adiciona aba Fidelidades no menu lateral

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from views.cliente_page import cliente_page
 from views.motoboy_page import motoboy_page
 from views.avaliacao_page import avaliacao_page
+from views.fidelidade_page import fidelidade_page
 
 # Futuramente, importar outras páginas:
-# from views.fidelidade_page import fidelidade_page
 # from views.campanha_page import campanha_page
 
 
@@ -17,7 +17,7 @@ st.set_page_config(
 st.sidebar.image("https://images.vexels.com/media/users/3/190242/isolated/preview/a865ab8bd0229080d9df607876ebbf16-pizza-plana-kawaii.png", width=150)
 st.sidebar.title("CRM Pizzaria Delivery")
 
-menu = ["Clientes", "Motoboys", "Avaliações"]
+menu = ["Clientes", "Motoboys", "Avaliações", "Fidelidades"]
 escolha = st.sidebar.selectbox("Menu Principal", menu)
 
 if escolha == "Clientes":
@@ -26,6 +26,8 @@ elif escolha == "Motoboys":
     motoboy_page()
 elif escolha == "Avaliações":
     avaliacao_page()
+elif escolha == "Fidelidades":
+    fidelidade_page()
 
 st.sidebar.markdown("---")
 st.sidebar.markdown("Desenvolvido por Amanda Taveira Amorim")
