f86fdc88a85afba234134a4decaaf2822132e993
AmandaAmorim <amanda.t.a99@gmail.com>
2025-06-04 22:08:32 -0300

    fix: adiciona super() para herdar atributos da classe Usuario

--- models/motoboy.py
+++ models/motoboy.py
@@ -1,22 +1,20 @@
-class Motoboy:
+from models.usuario import Usuario
+
+class Motoboy(Usuario):
     def __init__(self, id, nome, cpf, cnh, telefone, status_operacional,
                  zonas_atuacao=None, horarios_disponiveis=None, avaliacao_media=None, tempo_medio_entrega=None):
-        self.id = id
-        self.nome = nome
-        self.cpf = cpf
+        super().__init__(id, nome, cpf, telefone)
         self.cnh = cnh
         self.telefone = telefone
-        self.status_operacional = status_operacional  # Online / Offline
+        self.status_operacional = status_operacional  
         self.zonas_atuacao = zonas_atuacao or []
         self.horarios_disponiveis = horarios_disponiveis or []
         self.avaliacao_media = avaliacao_media or 0
         self.tempo_medio_entrega = tempo_medio_entrega or 0
 
     def to_dict(self):
-        return {
-            "id": self.id,
-            "nome": self.nome,
-            "cpf": self.cpf,
+        base = super().to_dict()
+        base.update({
             "cnh": self.cnh,
             "telefone": self.telefone,
             "status_operacional": self.status_operacional,
@@ -24,7 +22,8 @@ class Motoboy:
             "horarios_disponiveis": self.horarios_disponiveis,
             "avaliacao_media": self.avaliacao_media,
             "tempo_medio_entrega": self.tempo_medio_entrega
-        }
+        })
+        return base
 
     @staticmethod
     def from_dict(data):
