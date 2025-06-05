fc9756b326d24cec0b5c162b602d19dbfc769246
AmandaAmorim <amanda.t.a99@gmail.com>
2025-06-04 22:58:46 -0300

    fix: adiciona credenciais e criptografia

--- models/usuario.py
+++ models/usuario.py
@@ -1,7 +1,11 @@
+import hashlib
+
 class Usuario:
-    def __init__(self, id, nome, cpf, telefone):
+    def __init__(self, id, nome, senha, perfil,  cpf, telefone):
         self.id = id
         self.nome = nome
+        self.senha = senha
+        self.perfil = perfil
         self.cpf = cpf  
         self.telefone = telefone
 
@@ -9,6 +13,8 @@ class Usuario:
         return {
             "id": self.id,
             "nome": self.nome,
+            "senha": self.senha,
+            "perfil": self.perfil,
             "cpf": self.cpf,
             "telefone": self.telefone
         }
@@ -18,6 +24,14 @@ class Usuario:
         return Usuario(
             id=data.get("id"),
             nome=data.get("nome"),
+            senha=data.get("senha"),
+            perfil=data.get("perfil"),
             cpf=data.get("cpf"),
             telefone=data.get("telefone")
         )
+    @staticmethod
+    def gerar_hash(senha):
+        return hashlib.sha256(senha.encode()).hexdigest()
+    
+    def validar_senha(self, senha):
+        return self.senha == Usuario.gerar_hash(senha)
