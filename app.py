5b0b3a414eb157cbb554ade70bebcd18d08c4269
AmandaAmorim <amanda.t.a99@gmail.com>
2025-06-04 22:49:30 -0300

    fix: adiciona estrutura de selecao para login correspondente ao perfil do usuario

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from views.login import login_page
 from views.cliente_page import cliente_page
 from views.motoboy_page import motoboy_page
 from views.avaliacao_page import avaliacao_page
@@ -7,17 +8,31 @@ from views.campanha_page import campanha_page
 
 
 
-st.set_page_config(
-    page_title="CRM Pizzaria",
-    layout="wide"
-)
+if "logado" not in st.session_state:
+    st.session_state["logado"] = False
+
+
+if not st.session_state["logado"]:
+    login_page()
+else:
+    nome = st.session_state["usuario_nome"]
+    perfil = st.session_state["usuario_perfil"]
 
 #Barra lateral
 st.sidebar.image("https://images.vexels.com/media/users/3/190242/isolated/preview/a865ab8bd0229080d9df607876ebbf16-pizza-plana-kawaii.png", width=150)
 st.sidebar.title("CRM Pizzaria Delivery")
 
-menu = ["Clientes", "Motoboys", "Avaliações", "Fidelidades", "Campanhas"]
+if perfil == "Funcionário":
+   menu = ["Clientes", "Motoboys", "Avaliações", "Fidelidades", "Campanhas"]
+elif perfil == "Motoboy":
+    menu = ["Avaliações", "Sair"]
+elif perfil == "Cliente":
+    menu = ["Avaliações", "Sair"]
+else:
+    menu = ["Sair"]      
+
 escolha = st.sidebar.selectbox("Menu Principal", menu)
+
 
 if escolha == "Clientes":
     cliente_page()
