09622dbcfb57c5cd2848fcbe8bccc1d000677103
Lucas Moreira <112831891+Moreira-89@users.noreply.github.com>
2025-06-06 00:13:29 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,1 @@
-from .usuario import Usuario
-from .cliente import Cliente
-from .motoboy import Motoboy
-from .avaliacao import Avaliacao
-from .campanha import Campanha
-from .fidelidade import Fidelidade
-
-__all__ = [
-    "Usuario",
-    "Cliente",
-    "Motoboy",
-    "Avaliacao",
-    "Campanha",
-    "Fidelidade",
-]
 
