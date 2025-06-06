9158d2ba72870b3cbbc63b4e3e30b64ba203c837
Lucas Moreira <112831891+Moreira-89@users.noreply.github.com>
2025-06-06 00:03:00 -0300

    Update __init__.py

--- dao/__init__.py
+++ dao/__init__.py
@@ -1,16 +1,2 @@
-from .cliente_dao import ClienteDAO
-from .campanha_dao import CampanhaDAO
-from .fidelidade_dao import FidelidadeDAO
-from .motoboy_dao import MotoboyDAO
-from .avaliacao_dao import AvaliacaoDAO
-from .usuario_dao import UsuarioDAO
 
-__all__ = [
-    "ClienteDAO",
-    "CampanhaDAO",
-    "FidelidadeDAO",
-    "MotoboyDAO",
-    "AvaliacaoDAO",
-    "UsuarioDAO",
-]
 
